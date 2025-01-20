eb664b94a0c741903e819946be74077d02703168
EskilE <eskilme08@gmail.com>
2025-01-20 09:10:37 +0100

    added montly expense

--- Personal expense tracker.py
+++ Personal expense tracker.py
@@ -3,24 +3,27 @@ import os
 import datetime
 import matplotlib.pyplot as plt
 
-global amount
+global amount, expenses_file
 
 def database_cvs():
-    if not os.path.isfile("expenses.csv"):
-        with open("expenses.csv", mode='w') as file:
+    global expenses_file
+    expenses_file = "expenses.csv"
+
+    if not os.path.isfile(expenses_file):
+        with open(expenses_file, mode='w') as file:
             writer = csv.writer(file)
-            writer.writerow(["Date", "Category", "Amount"])
+            writer.writerow(["Date", "Category", "Amount"])      
 
 def add_expense():
-    global amount
-    
+    global amount, expenses_file
+
     print("\nWhat expense would you like to add?")
-    print("\n1. Food\n2. Transpotation\n3. Entertainment\n4. Bill\n5. Housing\n6. Other\n7. Back: ")
+    print("\n1. Food\n2. Transportation\n3. Entertainment\n4. Bill\n5. Housing\n6. Other\n7. Back: ")
     expenses_input = input("Enter the number of the expense: ")
     if expenses_input == "1":
         expenses = "Food"
     elif expenses_input == "2":
-        expenses = "Transpotation"
+        expenses = "Transportation"
     elif expenses_input == "3":
         expenses = "Entertainment"
     elif expenses_input == "4":
@@ -53,22 +56,18 @@ def add_expense():
 
     valid_amount()
 
-    with open("expenses.csv", mode='a', newline='') as file:
+    with open(expenses_file, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([date, expenses, amount]) 
     
     print("Expense added successfully!") 
     print("")
 
-    main = str(input("Want to go back to main (y/n): "))     
-    if main == "y":
-        main()
-    else:
-        exit()
-    
+    return_main()
+
 def view_expenses():
     try:
-        with open("expenses.csv", mode='r') as file:
+        with open(expenses_file, mode='r') as file:
             reader = csv.reader(file)
             next(reader) 
             print(f"{'Date':<15}{'Category':<15}{'Amount':<10}")
@@ -81,16 +80,12 @@ def view_expenses():
     except FileNotFoundError:
         print("Storage file does not exist\n")
     print("")
-    
-    Main = str(input("Want to go back to main (y/n): "))     
-    if Main == "y":
-        main()
-    else:
-        exit()
+
+    return_main()
 
 def generate_report():
     try:
-        with open("expenses.csv", mode='r') as file:
+        with open(expenses_file, mode='r') as file:
             reader = csv.reader(file)
             next(reader) 
             categories = {}
@@ -110,9 +105,30 @@ def generate_report():
     except FileNotFoundError:
         print("No expenses found. Add some first!\n")
 
+def monthly_expenses():
+    try:
+        month = input("What month do you want to see (mm-yyyy)?: ")
+
+        with open(expenses_file, mode="r") as file:
+            reader = csv.reader(file)
+            next(reader)
+            filtered = [row for row in reader if row[0].split('-')[1:] == month.split('-')]
+            if filtered:
+                print(f"\nExpenses for {month}:")
+                print(f"{'Date':<15}{'Category':<15}{'Amount':<10}")
+                print("-" * 40)
+                for row in filtered:
+                    print(f"{row[0]:<15}{row[1]:<15}${row[2]:<10}")
+            else:
+                print(f"No expense for {month}")
+    except FileExistsError:
+        print("Storage file does not exsist")
+
+    return_main()
+
 def total_expenses():
     
-    with open("expenses.csv", mode='r') as file:
+    with open(expenses_file, mode='r') as file:
         reader = csv.reader(file)
         next(reader) 
         food = 0
@@ -127,7 +143,7 @@ def total_expenses():
                 if row[1] == "Food":
                     food += float(row[2])
                     food = round(food, 2)
-                elif row[1] == "Transpotation":
+                elif row[1] == "Transportation":
                     transportation += float(row[2])
                     transportation = round(transportation, 2)
                 elif row[1] == "Entertainment":
@@ -150,7 +166,7 @@ def total_expenses():
         print("-" * 20)
 
     print(f"Food: ${food}")
-    print(f"Transpotation: ${transportation}")  
+    print(f"Transportation: ${transportation}")  
     print(f"Entertainment: ${entertainment}")
     print(f"Bill: ${bill}")
     print(f"Housing: ${housing}")
@@ -162,20 +178,16 @@ def total_expenses():
     else:
         print(f"Total: ${total}\n")
     
-    main = str(input("Want to go back to main (y/n): "))     
-    if main == "y":
-        main()
-    else:
-        exit()
+    return_main()
     
 def exit():
     print("Goodbye!")
 
 def main():
-    global Main
+    
     print("\n\n---Personal Expense Tracker---")
     print("-" * 30,)
-    print("\n 1. Add expense\n 2. View expenses\n 3. Generate report\n 4. Total\n 5. Exit\n")
+    print("\n 1. Add expense\n 2. View expenses\n 3. Generate report\n 4. Monthly Expenses\n 5. Total\n 6. Exit\n")
     categories = input("input here: ")
 
     if categories == "1":
@@ -186,12 +198,23 @@ def main():
         generate_report()
         main()
     elif categories == "4":
-        total_expenses()
+        monthly_expenses()
     elif categories == "5":
+        total_expenses()
+    elif categories == "6":
         exit()
     else:
         print("Invalid choice. Please try again.\n")
         main()
+
+def return_main():
+    choise = str(input("Want to go back to main (y/n): "))  
+    if choise == "y":
+        main()
+    elif choise == "n":
+        exit()
+    else:
+        print("invalid input")
 
 database_cvs()
 main()
